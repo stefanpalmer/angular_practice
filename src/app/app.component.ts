baefb5247ae46e368f2693b4241281d6a4ea7940
stefanpalmer <palmerstefan@yahoo.com>
2017-10-27 13:30:33 +0200

    add navigation by event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  emittedCategory = 'recipe';
+  
+  onNavigate(category: string) {
+    this.emittedCategory = category;
+  }
 }
