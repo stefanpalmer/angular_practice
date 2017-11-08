e03028e0e346546d1afa6c2fbe642b6e892025b9
stefanpalmer <palmerstefan@yahoo.com>
2017-11-08 19:25:28 +0100

    make add shopping item through service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,20 @@
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
 export class ShoppingListService {
-  
+  ingredientsAdded = new EventEmitter<Ingredient[]>();
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Oranges', 3),
+    new Ingredient('Flour', 1)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsAdded.emit(this.ingredients.slice());
+  }
 }
