599db5f752c7e128cd09e6c1dd4c12477e480f83
stefanpalmer <palmerstefan@yahoo.com>
2017-11-30 18:35:53 +0100

    add ingredient button and functionality

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -31,6 +31,15 @@ export class RecipeEditComponent implements OnInit {
 
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    );
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
