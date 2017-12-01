ad57aebc9e69f38f43ae65c089fcdfc538e1fc5a
stefanpalmer <palmerstefan@yahoo.com>
2017-12-01 07:11:59 +0100

    make submit recipe edit form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -28,7 +28,11 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-
+    if (this.editMode) {
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
   }
 
   onAddIngredient() {
